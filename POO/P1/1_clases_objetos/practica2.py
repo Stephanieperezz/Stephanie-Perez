9b8f2bd75b22a11206f923c8827adb3dcfd81e67
Stephanieperezz <stephanie_3141240114@utd.edu.mx>
2025-11-02 21:14:37 -0600

    Add files via upload

---
+++ POO/P1/1_clases_objetos/practica2.py
@@ -0,0 +1,35 @@
+""" EJERCICIO: #PRACTICA 2 MODELAR Y DIAGRAMAR EN POO"""
+import os
+os.system("cls")
+"""Lo que esta comentado # NO ES UN BUENA PRACTICA Y SOLO ES CUANDO LA CLASE ESTA EN PUBLICO"""
+#PROTEGIDO ES Q SI SE QUIERE IMPRIMIR DENTRO DEL COCHE1._COLOR, SI DEJA MOSTRAR EL VALOR TANTO EN PUBLIOC COMO PROTEGIDO PERO NO ES UNA UENA PRACTICA LO DE POTEGIFO
+class Coches:
+    def __init__(self, color, marca, velocidad): ##EN LUGAR DE SELF SE PUEDE USAR CUALQUIER OTRA PALABRA
+       self.__color=color
+       self.__marca=marca
+       self.__velocidad=velocidad  #__ ES PARA HACERLO PRIVADO, _ ES PROTEGIDO 
+    ##Metodos del objeto
+    def acelerar(self, incremento):
+        self.__velocidad=self.__velocidad+incremento
+        return self.__velocidad
+    def frenar(self, decremento):
+        self.__velocidad=self.__velocidad-decremento
+        return self.__velocidad
+    def tocar_claxon(self):
+        print("PIIIIIII")
+##Instanciar o crear objetos en la clase Coches
+#coche1=Coches()
+#coche1.marca#PARA QUE SE LE ASIGNE EL VALOR
+#print(coche1.marca)##Imprimirlo
+coche1=Coches("Blanco", "Toyota", 220)
+coche2=Coches("Rojo", "Honda", 180)
+print(coche1.acelerar(50))
+print(coche2.frenar(100))
+print(coche1.tocar_claxon())
+##ESTO ES ANTES DE HACERLOS PRIVADOS
+#220 a 270 180 a 80
+#coche1=Coches("Blanco", "Toyota", 220)
+print(f"Los valores del objeto 1 son: {coche1.__marca},{coche1.__color},{coche1.__velocidad}")
+#print(f"El coche 1 acelero de {coche1.__velocidad} a {coche1.acelerar(50)}")
+#print(f"Los valores del objeto 2 son: {coche2.__marca},{coche2.__color},{coche2.__velocidad}")
+#print(f"El coche 2 freno de {coche2.velocidad} a {coche2.frenar(100)}")
---
+++ POO/P1/1_clases_objetos/practica3.py
@@ -0,0 +1,64 @@
+"""Modelar y diagramar en sistema de scuela"""
+class Alumnos:
+    def __init__(self, nombre, edad, matricula):
+        self.__nombre=nombre
+        self.__edad=edad
+        self.__matricula=matricula
+    def get_nombre(self):
+        return self.__nombre
+    def get_edad(self):
+        return self.__edad
+    def get_matricula(self):
+        return self.__matricula
+    def inscribirse (self):
+        return "Estoy inscrito"
+    def estudiar (self):
+        pass
+
+
+Alumno2=Alumnos("Alondra", 19, 31412402)
+Alumno1 = Alumnos("Stephanie", 18, 31412401)
+print(f"Los valores de Alumno 1 son: {Alumno1.get_nombre()}, {Alumno1.get_edad()}, {Alumno1.get_matricula()}")
+print(Alumno1.inscribirse())
+print(f"Los valores de Alumno 2 son: {Alumno2.get_nombre()}, {Alumno2.get_edad()}, {Alumno2.get_matricula()}")
+print(Alumno2.inscribirse())
+class Cursos:
+    def __init__(self, nombre, codigo, creditos):
+        self.__nombre=nombre
+        self.__codigo=codigo
+        self.__creditos=creditos
+    def get_nombre(self):
+        return self.__nombre
+    def get_codigo(self):
+        return self.__codigo
+    def get_creditos(self):
+        return self.__creditos
+    def asignar():
+        pass
+
+
+Curso1=Cursos("Matematicas", 1234, 12)
+Curso2=Cursos("Programacion", 4321, 20)
+print(f"Los valores de curso 1 son: {Curso1.get_nombre()}, {Curso1.get_codigo()}, {Curso1.get_creditos()}")
+print(f"Los valores de curso 2 son: {Curso2.get_nombre()}, {Curso2.get_codigo()}, {Curso2.get_creditos()}")
+class Profesores:
+    def __init__(self, nombre, experiencia, num_profesor):
+        self.__nombre=nombre
+        self.__experiencia=experiencia
+        self.__num_profesor=num_profesor
+    def get_nombre(self):
+        return self.__nombre
+    def get_experiencia(self):
+        return self.__experiencia
+    def get_num_profesor(self):
+        return self.__num_profesor
+    def impartir(self):
+        pass
+    def evaluar(self):
+        pass
+
+
+Profesor1=Profesores("Dagoberto", 13, 30412401)
+Profesor2=Profesores("Omar", 15, 30412402)
+print(f"Los valores de profesor 1 son: {Profesor1.get_nombre()}, {Profesor1.get_experiencia()}, {Profesor1.get_num_profesor()}")
+print(f"Los valores de profesor 2 son: {Profesor2.get_nombre()}, {Profesor2.get_experiencia()}, {Profesor2.get_num_profesor()}")
