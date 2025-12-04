3d3562d513b5aaa688ce5ccced2f2c2cabe79ed1
Stephanieperezz <stephanie_3141240114@utd.edu.mx>
2025-12-04 08:55:01 -0600

    commit_04_12_25

--- POO/P3/coches_system/controller/funciones.py
+++ POO/P3/coches_system/controller/funciones.py
@@ -8,45 +8,15 @@ class Controlador:
     def insertar_coche(marca,color,modelo,velocidad,caballaje,plazas):
         respuesta=coches.Coches.insertar_autos(marca,color,modelo,velocidad,caballaje,plazas)
         Controlador.respuesta_sql("Insertar",respuesta)
-    
-    @staticmethod
-    def agregar_camioneta(marca,color,modelo,velocidad,caballaje,plazas,traccion,cerrada):
-        respuesta=coches.Coches.insertar_camionetas(marca,color,modelo,velocidad,caballaje,plazas,traccion,cerrada)
-        Controlador.respuesta_sql("Insertar",respuesta)
-
-    @staticmethod
-    def insertar_camion(marca,color,modelo,velocidad,caballaje,plazas,eje,capacidadcarga):
-        respuesta=coches.Coches.insertar_camiones(marca,color,modelo,velocidad,caballaje,plazas,eje,capacidadcarga)
-        Controlador.respuesta_sql("Insertar",respuesta)
 
     @staticmethod
     def actualizar(id,marca,color,modelo,velocidad,caballaje,plazas):
         respuesta=coches.Coches.actualizar_auto(id,marca,color,modelo,velocidad,caballaje,plazas)
         Controlador.respuesta_sql("Actualizar",respuesta)
-    
-    @staticmethod
-    def actualizar_camioneta(id,marca,color,modelo,velocidad,caballaje,plazas,traccion,cerrada):
-        respuesta=coches.Coches.actualizar_camionetas(id,marca,color,modelo,velocidad,caballaje,plazas,traccion,cerrada)
-        Controlador.respuesta_sql("Actualizar",respuesta)
-
-    @staticmethod
-    def actualizar_camion(id,marca,color,modelo,velocidad,caballaje,plazas,eje,capacidadcarga):
-        respuesta=coches.Coches.actualizar_camiones(id,marca,color,modelo,velocidad,caballaje,plazas,eje,capacidadcarga)
-        Controlador.respuesta_sql("Actualizar",respuesta)
 
     @staticmethod
     def eliminar(id):
         respuesta=coches.Coches.borrar_auto(id)
-        Controlador.respuesta_sql("Eliminar",respuesta)
- 
-    @staticmethod
-    def eliminar_camionetas(id):
-        respuesta=coches.Coches.borrar_camioneta(id)
-        Controlador.respuesta_sql("Eliminar",respuesta)
-
-    @staticmethod
-    def eliminar_camion(id):
-        respuesta=coches.Coches.borrar_camiones(id)
         Controlador.respuesta_sql("Eliminar",respuesta)
 
     @staticmethod
@@ -60,30 +30,9 @@ class Controlador:
         return registros
     
     @staticmethod
-    def mostrar_camionetas():
-        registros=coches.Coches.consultar_camionetas()
-        return registros
-    
-    @staticmethod
-    def editar_camioneta(id):
-        registros=coches.Coches.consultar_camionetas_editar(id)
-        return registros
-    
-    @staticmethod
-    def mostrar_camion():
-        registros=coches.Coches.consultar_camiones()
-        return registros
-    
-    @staticmethod
-    def editar_camion(id):
-        registros=coches.Coches.consultar_camiones_editar(id)
-        return registros
-    
-    @staticmethod
     def respuesta_sql(titulo,respuesta): 
         if respuesta:
             messagebox.showinfo(icon="info",title=titulo,message="Acción realizada con éxito...")
         else:
             messagebox.showerror(icon="info",title=titulo,message="No fue posible realizar la acción, vuelva a intentar")
-
 
